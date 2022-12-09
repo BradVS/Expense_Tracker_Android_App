351fdae319065d33dab82107676f6fe1f86fbe88
Bradley Stegbauer <bstegba@gmail.com>
2022-12-08 21:37:15 -0600

    JavaDoc for DateInfoConsumer method.

--- app/src/main/java/com/c323proj9/bradleystegbauer/datepicker/DateInfoConsumer.java
+++ app/src/main/java/com/c323proj9/bradleystegbauer/datepicker/DateInfoConsumer.java
@@ -1,5 +1,9 @@
 package com.c323proj9.bradleystegbauer.datepicker;
 
 public interface DateInfoConsumer {
+    /**
+     * Accepts the date String that is passed into it for use by the imeplementing class.
+     * @param dateString yyyy-MM-dd format String of the date that is passed in.
+     */
     void getDateString(String dateString);
 }
